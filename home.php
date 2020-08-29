a5c392dd8539ea9bf896f5704120a48a38cf8a8c
pianista30 <70445222+pianista30@users.noreply.github.com>
2020-08-29 11:59:12 -0500

    Estos son los archivos base con los que estaremos trabajando
    Utiliza Bootstrap y por el momento solo se encuentra la pagina home. 
    Home es la pagina principal y es solo un borrador muestra de lo que se quiere hacer.

---
+++ home.php
@@ -0,0 +1,325 @@
+<?php
+
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITSL - Academia de Inglés</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="icon" href="imagenes/itsl2.png">
+    <style type="text/css">
+        body{
+            font: 14px;
+            font-family:sans-serif;
+            background-image: linear-gradient(to bottom, #0a6d7a 580px, white  30%);
+            background-size: cover;
+            background-repeat: no-repeat;
+            }
+        header{
+            background-color: #000000;
+               }
+       
+        footer{
+            background: #000000;
+              }
+    
+        .baner{
+        background: #15859A;
+        color: white;
+		
+        
+              }
+        
+        .foto{
+             background: #15859A;
+        
+             }
+        a:link{
+            color: white;
+            padding: 16px;
+            padding-top: 20px;
+              }
+        a:visited{
+            color: white;
+            
+              }
+        a:hover {
+            color: lightgray;
+            text-decoration: none;
+              }
+        .menu{
+            padding: 15px 0 0 0;
+            text-align: center;
+             }
+        .menuImagen{
+            text-align: center;
+            padding: 3px;
+        }
+        .lista{
+            padding-left: 10px;
+            color: white;
+            font-size: 13px;
+            text-align: left;
+              }
+        .contenedor{
+            position: relative;
+            width: 100%;
+              }
+        .mainfoto{
+            border-top-left-radius: 8px; 
+            border-bottom-left-radius: 8px;
+            width: 100%;
+            height: auto;
+                 }
+        .subtitulo{
+            position: relative;
+            left:15px;
+            bottom: 50px;
+            padding: 13px 20px 13px 20px;
+            width: 96%;
+            color: white;
+            background: black;
+            opacity: .75;
+            border-radius: 8px;
+                 }
+        .redes{
+            padding-left: 10px;
+            color: white;
+            font-size: 13px;
+            line-height: 1.7;
+                 }
+        .tituloRedes{
+            color: lightgray;
+            padding-left: 10px;
+        }
+        .acerca{
+            color: white;
+            font-size: 13px;
+            padding-left: 10px;
+            line-height: 1.7;
+               }
+        .pie{
+            color: white;
+            font-size: 13px;
+            
+            line-height: 1.7;
+            padding-bottom: 30px;
+         }
+        .iconos{
+            width: 18px;
+            height: auto;
+            padding-bottom: 3px;
+        }
+        .bordes{
+            border-right-style:dotted; 
+            border-right-color:lightgray; 
+            border-right-width: 1px; 
+            margin: 20px 0 20px 0;
+        }
+        .remove-padding{
+            padding:  0;
+            margin:  0;
+        }
+        @media (max-width: 980px){
+            .subtitulo{
+                opacity: 1;
+                bottom: 3px;
+                left:10px;
+            }
+            .lista{
+                margin-left: 30px;
+            }
+        }
+
+
+
+
+    </style>
+</head>
+    
+    
+<body>
+    
+    <header>
+        
+         <div class="container-fluid" >
+             <div class="row">
+		          <div class="col-sm-4 menuImagen" >
+                        <img src="imagenes/itslnobreLargo.png" width="70%" height="auto">
+                  </div>
+                  <div class="col-sm-4" >
+                          <div class="row">
+                             <div class="col-sm-4" >
+                                 
+                                    <p class="menu">
+                                      <a href="reset-password.php" >Estudiantes</a>
+                                      </p>
+                                 
+                              </div>
+                              <div class="col-sm-4">
+                                 
+                                    <p class="menu">
+                                      <a href="logout.php" >Docentes</a>
+                                     </p>
+                                 
+                              </div>
+                              <div class="col-sm-4">
+                                 
+                                    <p class="menu">
+                                     <a href="logout.php" >Administración</a></p>
+                                 
+                              </div>
+                            </div>
+                  </div>
+                 
+                 <div class="col-sm-4" >
+                     
+                     <!-- Columna vacia -->
+                     
+                 </div>
+            </div>
+        </div>
+        
+    </header>
+    
+    
+    <div class="container-fluid">
+        <div class="row" style="padding-bottom: 10px; padding-top: 20px; background:#0a6d7a">
+            <div class="col-sm-4" style="text-align:center; padding-bottom:20px; ">
+                <img src="imagenes/TecNMwhite.png" width="200px" height="auto" >
+                
+            </div>
+            
+            <div class="col-sm-2" >
+                
+                
+            </div>
+            
+            <!-- inicio de las listas -->
+            
+            <div class="col-sm-3">
+                <div class="lista">
+                        <p  style="line-height: 2;">
+                            > &nbsp; Superación personal<br>
+                            > &nbsp; Mejores oportunidades<br>
+                            > &nbsp; Profecionales de calidad<br>
+                            > &nbsp; Bolsa de trabajo<br>
+                            > &nbsp; Al alcance de tu mano
+                        </p>
+                </div>
+                
+            </div>
+             <div class="col-sm-3" >
+                <div class="lista">
+                        <p  style="line-height: 2;">
+                            > &nbsp; Clases dinamicas<br>
+                            > &nbsp; Desarrollo artistico<br>
+                            > &nbsp; Evaluación TOEFL<br>
+                            > &nbsp; Viajes educativos<br>
+                            > &nbsp; Becas
+                        </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+
+    <div class="container contenedor">
+        <div class="row " >
+            <div class="col-sm-12 ">
+                <div class="row">
+                    <div class="col-sm-8 remove-padding">
+                            <img class="mainfoto" src="imagenes/study.jpg" >
+                    </div>
+                    <div class="col-sm-4" style=" background-color:#15859A; border-top-right-radius: 8px; border-bottom-right-radius: 8px;">
+                        <div >
+                            <p style=" color:white; font-size: 2vw;  font-family:Verdana; text-align:center; transform: scale(.9, 1);"><br>"English is a universal language used in many countries, including México. ITSL English Academy is offering classes as part of your curriculum"<br></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12" style="text-align: left;">
+                        <p class="subtitulo">ITSL - Academia de Inglés</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid">
+        <div class="row" >
+                      <div class="col-sm-12" >
+                            <img src="imagenes/itslnobreLargo.png" width="100%" height="auto" style="padding-left:80px; padding-right:20px; padding-top:5px;">
+                      </div>
+        </div>
+    </div>    
+         
+    
+     <div class="container-fluid">
+         <div class="row" style="background:#0a6d7a; ">
+            <div class="col-md-3 bordes">
+                <p>  </p>
+            </div>
+            <div class="col-md-6 bordes" >
+
+                <div class="row">
+                    <div class="col-md-6" >
+                    </div>
+                    
+                    <div class="col-md-6 " >
+                         <p class="tituloRedes">Siguenos en redes sociales</p>
+                         <p class="redes">
+                            <img class="iconos" src="imagenes/face.png">&emsp;Facebook<br>
+                            <img class="iconos" src="imagenes/insta.png">&emsp;Instagram<br>
+                            <img class="iconos" src="imagenes/twi.png">&emsp;Twitter<br>
+                            <img class="iconos" src="imagenes/yt.png">&emsp;YouTube<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        
+             
+        <div class="col-md-3" style=" margin: 20px 0 20px 0;">
+            <p style="color: lightgray; padding-left:10px;">Acerca de está Página Web</p>
+            <p class="acerca">
+                Cookies<br>
+                Privacy policy<br>
+            </p>
+        </div>
+    </div>
+    
+    </div>
+    
+    
+
+        
+
+        
+        
+
+<footer>
+        <div class="container-fluid">
+         <div class="row" style="background: black; color:white; text-align:center;">
+            <div class="col-md-3 bordes ">
+                <p><img src="imagenes/itslnobreLargo.png" style="width:60%; height:auto; padding-top:20px; "></p>
+                <p class="pie">© 2020 ITSL - English Academy</p>
+            </div>
+            <div class="col-md-6 bordes">
+                <p> </p>
+            </div>
+            <div class="col-md-3 bordes">
+                <p></p>
+            </div>
+        </div>
+    
+    
+    </div>
+ 
+
+</footer>
+
+</body>
+</html>
