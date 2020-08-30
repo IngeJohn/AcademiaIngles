74595011338f720d968005f1fd59040bde24f791
davidcini <davidgugas@hotmail.com>
2020-08-29 21:50:41 -0500

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
 
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,21 +20,21 @@
         header{
             background-color: #000000;
                }
-       
+
         footer{
             background: #000000;
               }
-    
+
         .baner{
         background: #15859A;
         color: white;
-		
-        
+
+
               }
-        
+
         .foto{
              background: #15859A;
-        
+
              }
         a:link{
             color: white;
@@ -43,7 +43,7 @@
               }
         a:visited{
             color: white;
-            
+
               }
         a:hover {
             color: lightgray;
@@ -68,7 +68,7 @@
             width: 100%;
               }
         .mainfoto{
-            border-top-left-radius: 8px; 
+            border-top-left-radius: 8px;
             border-bottom-left-radius: 8px;
             width: 100%;
             height: auto;
@@ -103,7 +103,7 @@
         .pie{
             color: white;
             font-size: 13px;
-            
+
             line-height: 1.7;
             padding-bottom: 30px;
          }
@@ -113,9 +113,9 @@
             padding-bottom: 3px;
         }
         .bordes{
-            border-right-style:dotted; 
-            border-right-color:lightgray; 
-            border-right-width: 1px; 
+            border-right-style:dotted;
+            border-right-color:lightgray;
+            border-right-width: 1px;
             margin: 20px 0 20px 0;
         }
         .remove-padding{
@@ -138,12 +138,12 @@
 
     </style>
 </head>
-    
-    
+
+
 <body>
-    
+
     <header>
-        
+
          <div class="container-fluid" >
              <div class="row">
 		          <div class="col-sm-4 menuImagen" >
@@ -152,64 +152,64 @@
                   <div class="col-sm-4" >
                           <div class="row">
                              <div class="col-sm-4" >
-                                 
+
                                     <p class="menu">
                                       <a href="reset-password.php" >Estudiantes</a>
                                       </p>
-                                 
+
                               </div>
                               <div class="col-sm-4">
-                                 
+
                                     <p class="menu">
                                       <a href="logout.php" >Docentes</a>
                                      </p>
-                                 
+
                               </div>
                               <div class="col-sm-4">
-                                 
+
                                     <p class="menu">
                                      <a href="logout.php" >Administración</a></p>
-                                 
+
                               </div>
                             </div>
                   </div>
-                 
+
                  <div class="col-sm-4" >
-                     
+
                      <!-- Columna vacia -->
-                     
+
                  </div>
             </div>
         </div>
-        
+
     </header>
-    
-    
+
+
     <div class="container-fluid">
         <div class="row" style="padding-bottom: 10px; padding-top: 20px; background:#0a6d7a">
             <div class="col-sm-4" style="text-align:center; padding-bottom:20px; ">
                 <img src="imagenes/TecNMwhite.png" width="200px" height="auto" >
-                
+
             </div>
-            
+
             <div class="col-sm-2" >
-                
-                
+
+
             </div>
-            
+
             <!-- inicio de las listas -->
-            
+
             <div class="col-sm-3">
                 <div class="lista">
                         <p  style="line-height: 2;">
                             > &nbsp; Superación personal<br>
                             > &nbsp; Mejores oportunidades<br>
-                            > &nbsp; Profecionales de calidad<br>
+                            > &nbsp; Profesionales de calidad<br>
                             > &nbsp; Bolsa de trabajo<br>
                             > &nbsp; Al alcance de tu mano
                         </p>
                 </div>
-                
+
             </div>
              <div class="col-sm-3" >
                 <div class="lista">
@@ -224,8 +224,8 @@
             </div>
         </div>
     </div>
-    
-    
+
+
 
     <div class="container contenedor">
         <div class="row " >
@@ -248,16 +248,16 @@
             </div>
         </div>
     </div>
-    
+
     <div class="container-fluid">
         <div class="row" >
                       <div class="col-sm-12" >
                             <img src="imagenes/itslnobreLargo.png" width="100%" height="auto" style="padding-left:80px; padding-right:20px; padding-top:5px;">
                       </div>
         </div>
-    </div>    
-         
-    
+    </div>
+
+
      <div class="container-fluid">
          <div class="row" style="background:#0a6d7a; ">
             <div class="col-md-3 bordes">
@@ -268,7 +268,7 @@
                 <div class="row">
                     <div class="col-md-6" >
                     </div>
-                    
+
                     <div class="col-md-6 " >
                          <p class="tituloRedes">Siguenos en redes sociales</p>
                          <p class="redes">
@@ -280,8 +280,8 @@
                     </div>
                 </div>
             </div>
-        
-             
+
+
         <div class="col-md-3" style=" margin: 20px 0 20px 0;">
             <p style="color: lightgray; padding-left:10px;">Acerca de está Página Web</p>
             <p class="acerca">
@@ -290,15 +290,15 @@
             </p>
         </div>
     </div>
-    
+
     </div>
-    
-    
 
-        
 
-        
-        
+
+
+
+
+
 
 <footer>
         <div class="container-fluid">
@@ -314,10 +314,10 @@
                 <p></p>
             </div>
         </div>
-    
-    
+
+
     </div>
- 
+
 
 </footer>
 
